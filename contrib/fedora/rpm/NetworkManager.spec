zstamas97/NetworkManager

commit bb832641ebb62a0e24b5c67fc88d153a9a46c2ad

rpm: remove build-time default for plugins on newer distros
On newer distros, remove the build-time default for settings
plugins. All plugins found in the plugin directory will be used.

contrib/fedora/rpm/NetworkManager.spec
@@ -153,9 +153,9 @@
 %endif
 
 %if 0%{?rhel} > 8 || 0%{?fedora} > 32
-%global config_plugins_default keyfile,ifcfg-rh
+%global config_plugins_default_ifcfg_rh 0
 %else
-%global config_plugins_default ifcfg-rh
+%global config_plugins_default_ifcfg_rh 1
 %endif
 
 %if 0%{?fedora}
@@ -699,7 +699,9 @@ Preferably use nmcli instead.
 	-Dfirewalld_zone=false \
 %endif
 	-Ddist_version=%{version}-%{release} \
-	-Dconfig_plugins_default=%{config_plugins_default} \
+%if %{?config_plugins_default_ifcfg_rh}
+	-Dconfig_plugins_default=ifcfg-rh \
+%endif
 	-Dresolvconf=no \
 	-Dnetconfig=no \
 	-Dconfig_dns_rc_manager_default=%{dns_rc_manager_default} \

commit 2d3b17315451dde395b815cd6a8e9e051ef144ed

rpm: fix autotools build options for default plugins
Fixes: bb832641ebb6 ('rpm: remove build-time default for plugins on newer distros')

contrib/fedora/rpm/NetworkManager.spec
@@ -867,7 +867,9 @@ intltoolize --automake --copy --force
 	--disable-firewalld-zone \
 %endif
 	--with-dist-version=%{version}-%{release} \
-	--with-config-plugins-default=%{config_plugins_default} \
+%if %{?config_plugins_default_ifcfg_rh}
+	--with-config-plugins-default=ifcfg-rh \
+%endif
 	--with-resolvconf=no \
 	--with-netconfig=no \
 	--with-config-dns-rc-manager-default=%{dns_rc_manager_default} \